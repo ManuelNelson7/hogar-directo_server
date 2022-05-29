6e75a03e2081ca60e1a1d2ea00cc43ebcc39b149
ManuelNelson7 <manuel.nelson7@hotmail.com>
2022-05-29 00:25:19 -0300

    schemas

--- schemas/person.js
+++ schemas/person.js
@@ -1,41 +1,46 @@
 export default {
-    name: 'person',
-    title: 'Person',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-        description: 'Please use "Firstname Lastname" format',
-      },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-          source: 'name',
-          maxLength: 100,
-        },
-      },
-      {
-        name: 'id',
-        title: 'ID',
-        type: 'number',
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-            hotspot: true,
-        },
-      },
-    ],
-    preview: {
-      select: {
-        title: 'name',
-        media: 'image',
+  name: 'person',
+  title: 'Person',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description: 'Please use "Firstname Lastname" format',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
       },
     },
-  }
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+}
